5a476a70fbefc5f7e8f4fab3c2fcfd356549c2d9
Dresden Feitzinger <dfeitzinger@comcast.net>
2018-01-06 16:46:10 -0500

    changed max us sensor distance; assigned names to constants for clarity

--- US_CODE/US_Distance_Code.py
+++ US_CODE/US_Distance_Code.py
@@ -4,6 +4,9 @@ GPIO.setmode(GPIO.BCM)                     #Set GPIO pin numbering
 
 TRIG = 23                                  #Associate pin 23 to TRIG
 ECHO = 24                                  #Associate pin 24 to ECHO
+MAX_DISTANCE = 575                         #Maximum ultrasonic sensor distance
+MIN_DISTANCE = 2                           #Minimum ultrasonic sensor distance
+HALF_SPEED_OF_SOUND = 17150 
 
 print "Distance measurement in progress"
 
@@ -11,7 +14,6 @@ GPIO.setup(TRIG,GPIO.OUT)                  #Set pin as GPIO out
 GPIO.setup(ECHO,GPIO.IN)                   #Set pin as GPIO in
 
 while True:
-
   GPIO.output(TRIG, False)                 #Set TRIG as LOW
   print "Waitng For Sensor To Settle"
   time.sleep(2)                            #Delay of 2 seconds
@@ -28,10 +30,10 @@ while True:
 
   pulse_duration = pulse_end - pulse_start #Get pulse duration to a variable
 
-  distance = pulse_duration * 17150        #Multiply pulse duration by 17150 to get distance
+  distance = pulse_duration * HALF_SPEED_OF_SOUND           #Multiply pulse duration by 17150 to get distance
   distance = round(distance, 2)            #Round to two decimal points
 
-  if distance > 2 and distance < 400:      #Check whether the distance is within range
+  if distance > MIN_DISTANCE and distance < MAX_DISTANCE:   #Check whether the distance is within range
     print "Distance:",distance - 0.5,"cm"  #Print distance with 0.5 cm calibration
   else:
     print "Out Of Range"                   #display out of range
