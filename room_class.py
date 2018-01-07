0dfe4f765945297d5ae70681d8d9ae815ca370cd
Dresden Feitzinger <dfeitzinger@comcast.net>
2018-01-06 19:02:12 -0500

    refactored rectangle class; created Room class; created circle class

--- room_class.py
+++ room_class.py
@@ -5,6 +5,8 @@
 #Programmer Name: Matthew Marschall 
 #========================================
 
+import numpy as np
+
 class Point:
 # Class to describe coord points
         def __init__(self, x=0, y=0):
@@ -19,47 +21,63 @@ class Point:
 		
         def get_y(self):
                 return self.y 
-		
-class Rectangle:
-# A class to manufacture rectangle objects 
 
-        def __init__(self, posn, w, h):
-        #Initialize rectangle at posn, with width w, height h
-                self.corner = posn
-                self.width = w
-                self.height = h
+class Room():
+    ''' Superclass for room types '''
+    def __init__(self, t):
+        #--- Public -----------
+        self.type = t
+        self.area = None
+        #----------------------
 
-        def __str__(self):
-                return  "({0}, {1}, {2})".format(self.corner, self.width, self.height)
-	
-        def grow(self, delta_width, delta_height):
-        # Grow (or shrink) this object by the deltas 
-                self.width += delta_width
-                self.height += delta_height
-	
-        def get_width(self):
-                return self.width
+    def set_area(self, area):
+        ''' Set room area '''
+        self.area = area
+
+class Circle(Room):
+    ''' Create a room of type circle '''
+    def __init__(self, r):
+        ''' Takes one positional argument: radius '''
+        #--- Initialize Superclass -------
+        t = 1
+        Room.__init__(t)
+        self.set_area(4*np.pi*(r**2))
+
+        #--- Public ----------------------
+        self.diameter = r*2
+        self.radius   = r
+        #---------------------------------
+
+    def get_radius(self):
+        ''' Return radius of the circle in centimeters '''
+        return self.radius
+
+    def calc_circumference(self):
+        ''' Calculate the circumference of the room '''
+        return 2 * np.pi * self.radius
 		
-        def get_height(self):
-                return self.height
-		
-        def move(self, dx, dy):
-        #Move this object by the deltas
-                self.corner.x += dx
-                self.corner.y += dy
-		
-        def same_coordinates(self, p1, p2):
-                return (p1.x == p2.x) and (p1.y == p2.y)
-		
-        def area(self):
-                return self.width*self.height
-		
-        def perimeter(self):
-                return ((2 * self.width) + (2 * self.height))
-	
-        def flip(self):
-                self.width, self.height = self.height, self.width
-	
-        def contains(self, point):
-                x, y = point.get_x(), point.get_y()
-                return 0 <= x < self.width and 0 <= y < self.height
+class Rectangle(Room):
+    ''' Create a room of type rectangle '''
+    def __init__(self, w, h):
+        #--- Initialize Superclass -------
+        t = 0
+        Room.__init__(t)
+        self.set_area(w*h)
+        #---------------------------------
+
+        #--- Public ----------------------
+        self.width = w
+        self.height = h
+        #---------------------------------
+
+    def get_width(self):
+        ''' Return rectangle width in centimeters '''
+        return self.width
+
+    def get_height(self):
+        ''' Return rectangle height in centimeters '''
+        return self.height
+
+    def calc_perimeter(self):
+        ''' Return rectangle perimeter in centimeters '''
+        return ((2 * self.width) + (2 * self.height))
